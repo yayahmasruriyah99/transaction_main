cd3261eadfd7169c2c821582a661f5705cb27033
yayahmasruriyah99 <yayah99masruriyah@gmail.com>
2024-04-13 20:54:17 +0700

    update terbaru

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
+const cors = require('cors')
+app.use(cors())
 const port = process.env.SERVER_PORT || 4000
 const path = require('path')
 
